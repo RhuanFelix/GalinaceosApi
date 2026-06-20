7b688017aa77b3dcf51b1948b890d2cb7126205c
Rhuan dos Santos Félix <rhuansantosfelix@gmail.com>
2026-06-20 16:22:26 -0300

    refactor: altera os nomes das variáveis de avicultores para sistemaCriacao

--- controllers/SistemaCriacaoController.py
+++ controllers/SistemaCriacaoController.py
@@ -20,27 +20,27 @@ def getSistemasCriacoes():
     return [sc.toDict() for sc in sistemaCriacao], 200
 
 @sistema_criacao_bp.post("/")
-def postAvicultores():
+def postSistemaCriacao():
     try:
         data = SistemaCriacaoSchema().load(request.get_json())
-        avicultor = SistemaCriacaoService().create(data)
-        return avicultor.toDict(), 201
+        sistemaCriacao = SistemaCriacaoService().create(data)
+        return sistemaCriacao.toDict(), 201
     except ValidationError as err:
         return jsonify(err.messages), 400
     
 @sistema_criacao_bp.put("/<int:id>")
-def putAvicultores(id: int):
+def putSistemaCriacao(id: int):
     try:
         data = SistemaCriacaoSchema().load(request.get_json())
-        avicultor = SistemaCriacaoService().update(id, data)
-        if avicultor is None:
+        sistemaCriacao = SistemaCriacaoService().update(id, data)
+        if sistemaCriacao is None:
             return {"mensagem": "O sistema de criação não foi encontrado"}, 404
-        return avicultor.toDict(), 200
+        return sistemaCriacao.toDict(), 200
     except ValidationError as err:
         return jsonify(err.messages), 400
     
 @sistema_criacao_bp.delete("/<int:id>")
-def deleteAvicultores(id: int):
+def deleteSistemaCriacao(id: int):
     removido = SistemaCriacaoService().delete(id)
     if not removido:
         return {"mensagem": "O sistema de criação não foi encontrado"}, 404
